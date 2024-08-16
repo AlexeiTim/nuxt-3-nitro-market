6806dd5551486d48fa6a13d0923b9e85ae64e42e
timashkov <timashkov060896@gmail.com>
2024-08-16 15:35:02 +0300

    формально добавил иконку

--- components/App/Logo.vue
+++ components/App/Logo.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <NuxtLink to="/">
-    Logo
-    <!-- <NuxtImg style="width: 100px" alt="Element logo" /> -->
+  <NuxtLink
+    to="/"
+    class="flex items-center justify-center gap-2 hover:text-red-200 transition-all duration-300"
+  >
+    <ElIcon>
+      <WindPower />
+    </ElIcon>
+    <p>Market</p>
   </NuxtLink>
 </template>
 
