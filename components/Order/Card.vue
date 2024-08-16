d7c5a39487f94e021d1e4f86245d35a592671891
timashkov <timashkov060896@gmail.com>
2024-08-16 16:01:20 +0300

    декомпозиция

---
+++ components/Order/Card.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { Order } from "~/types/order";
+
+defineProps<{
+  order: Order;
+}>();
+
+const ordersStore = useOrdersStore();
+
+const { $moment } = useNuxtApp();
+</script>
+
+<template>
+  <ElCard>
+    <div class="flex justify-between">
+      <div>
+        <p>
+          Date order:
+          {{ $moment(order.created_at).format("DD.MM.YY HH:mm:ss") }}
+        </p>
+        <div>
+          <h3>Cunsumer</h3>
+          <p>Firstname: {{ order.first_name }}</p>
+          <p>Lastname: {{ order.last_name }}</p>
+        </div>
+      </div>
+      <ElTooltip content="Cancel order" placement="top">
+        <ElButton
+          icon="Delete"
+          type="danger"
+          circle
+          @click="ordersStore.cancelOrder(order.id)"
+        />
+      </ElTooltip>
+    </div>
+  </ElCard>
+</template>
+
+<style scoped></style>
--- pages/Orders.vue
+++ pages/Orders.vue
@@ -1,37 +1,14 @@
 <script setup lang="ts">
 import { useOrdersStore } from "~/stores/orders-store";
 
-const oridersStore = useOrdersStore();
-const { $moment } = useNuxtApp();
+const ordersStore = useOrdersStore();
 </script>
 
 <template>
   <div class="flex flex-col gap-5">
     <h1 class="text-center">Orders</h1>
-    <template v-if="oridersStore.ordersCount">
-      <ElCard v-for="order in oridersStore.orders" :key="order.id">
-        <div class="flex justify-between">
-          <div>
-            <p>
-              Date order:
-              {{ $moment(order.created_at).format("DD.MM.YY HH:mm:ss") }}
-            </p>
-            <div>
-              <h3>Cunsumer</h3>
-              <p>Firstname: {{ order.first_name }}</p>
-              <p>Lastname: {{ order.last_name }}</p>
-            </div>
-          </div>
-          <ElTooltip content="Cancel order" placement="top">
-            <ElButton
-              icon="Delete"
-              type="danger"
-              circle
-              @click="oridersStore.cancelOrder(order.id)"
-            />
-          </ElTooltip>
-        </div>
-      </ElCard>
+    <template v-if="ordersStore.ordersCount">
+      <OrderCard v-for="order in ordersStore.orders" :order="order" />
     </template>
     <div v-else class="flex items-center justify-center">
       <ElTag>Empty</ElTag>
